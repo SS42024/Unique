0c5d871d17233762def966505d07f1d5deed8bcd
Shailesh Sharavana <shailesh.sharavana@gmail.com>
2024-11-05 12:39:02 -0800

    Initial Commit

---
+++ Unique/UniqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniqueApp.swift
+//  Unique
+//
+//  Created by shailesh sharavana on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UniqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniqueTests/UniqueTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniqueTests.swift
+//  UniqueTests
+//
+//  Created by shailesh sharavana on 11/5/24.
+//
+
+import Testing
+@testable import Unique
+
+struct UniqueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
